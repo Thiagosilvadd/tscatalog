95f6f36363b0a815359efcc02735f2930c73ea7c
thiagodd <silvaa.thiagod2@gmail.com>
2022-11-12 10:18:43 -0300

    First commit

---
+++ tscatalog/src/main/java/com/thiagodd/tscatalog/TscatalogApplication.java
@@ -0,0 +1,13 @@
+package com.thiagodd.tscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TscatalogApplication.class, args);
+	}
+
+}
---
+++ tscatalog/src/test/java/com/thiagodd/tscatalog/TscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagodd.tscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
