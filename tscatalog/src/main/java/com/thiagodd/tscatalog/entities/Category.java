99322ad711ecd045d1397781af993dd49c2accbe
thiagodd <silvaa.thiagod2@gmail.com>
2022-11-12 11:18:35 -0300

    CategoryResource

---
+++ tscatalog/src/main/java/com/thiagodd/tscatalog/entities/Category.java
@@ -0,0 +1,50 @@
+package com.thiagodd.tscatalog.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Category implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 7677181661226600999L;
+
+    private Long id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category category)) return false;
+
+        return getId() != null ? getId().equals(category.getId()) : category.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
+    }
+}
